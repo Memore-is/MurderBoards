fe2070487dbcc2be448533cc3841d3e9d1c36eb5
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-05-09 19:59:37 -0400

    updated dialogue, format, add folders and files as buttons

--- Murder Boards/Scripts/game.gd
+++ Murder Boards/Scripts/game.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var window = $Window
+@onready var terminal = $Terminal
 
 func _ready():
 	DialogueManager.show_dialogue_balloon(preload("res://Dialogue/intro.dialogue"), "start")
@@ -11,6 +12,8 @@ func _process(delta):
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-
 func _on_folder_pressed():
 	window.visible = true
+
+func _on_terminal_pressed():
+	terminal.visible = true
