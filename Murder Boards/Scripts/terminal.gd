c5349a254cba8880206441b9c875d2514dd11d68
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-05-10 04:54:56 -0400

    ehh somewhat works
    can see text and stuff but scene not chnaging right

--- Murder Boards/Scripts/terminal.gd
+++ Murder Boards/Scripts/terminal.gd
@@ -7,9 +7,16 @@ extends "res://Scripts/window.gd"
 func _ready():
 	output.append_text("Enter Password: \n")
 
-func add_line(text):
-	output.append_text(text + "\n")
-
 func _on_input_text_submitted(new_text):
-	add_line("> " + new_text)
+	output.append_text("> " + new_text + "\n")
+	
+	if new_text == "Z167R2968":
+		output.append_text("Correct Password./nDoor unlocking...")
+		get_tree().change_scene_to_file("res://Scenes/free.tscn")
+	else:
+		output.append_text("Wrong Password. Try Again.")
+		
+	await get_tree().process_frame
+	scroll.scroll_vertical = scroll.get_v_scroll_bar().max_value
+	
 	input.clear()
