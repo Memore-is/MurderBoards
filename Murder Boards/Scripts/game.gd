c2b051694321746b8cb826408d1de0d3287a85b5
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-05-10 02:31:21 -0400

    create new scene for each app

--- Murder Boards/Scripts/game.gd
+++ Murder Boards/Scripts/game.gd
@@ -1,19 +1,20 @@
 extends Control
 
-@onready var window = $Window
+@onready var folder = $Folder
 @onready var terminal = $Terminal
+@onready var trash = $Trash
 
 func _ready():
 	DialogueManager.show_dialogue_balloon(preload("res://Dialogue/intro.dialogue"), "start")
-
-func _process(delta):
-	pass
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_folder_pressed():
-	window.visible = true
+	folder.visible = true
 
 func _on_terminal_pressed():
 	terminal.visible = true
+
+func _on_trash_pressed():
+	trash.visible = true;
