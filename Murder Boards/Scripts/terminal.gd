6e8335ccfe5ed75f36e896b0eb5385cd6955b1d5
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-05-10 05:49:22 -0400

    WORKSSSSSSSS!!!

--- Murder Boards/Scripts/terminal.gd
+++ Murder Boards/Scripts/terminal.gd
@@ -14,9 +14,8 @@ func _on_input_text_submitted(new_text):
 		output.append_text("Correct Password./nDoor unlocking...")
 		get_tree().change_scene_to_file("res://Scenes/free.tscn")
 	else:
-		output.append_text("Wrong Password. Try Again.")
-		
-	await get_tree().process_frame
+		output.append_text("Wrong Password. Try Again.\n")
+		await get_tree().process_frame
+
 	scroll.scroll_vertical = scroll.get_v_scroll_bar().max_value
-	
 	input.clear()
